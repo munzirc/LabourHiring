8f9a177c8036621a43c7b86963d1e9b9c34ca15d
Mahammad Munzir <munzircnr710@gmail.com>
2023-10-02 13:15:50 +0530

    second

--- LabourHiring/src/main/java/com/labourhiring/springbootproject/LabourHiring/dao/Test.java
+++
@@ -1,9 +0,0 @@
-package com.labourhiring.springbootproject.LabourHiring.dao;
-
-public class Test {
-
-	public Test() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- LabourHiring/src/main/java/com/labourhiring/springbootproject/LabourHiring/dao/Test2.java
+++
@@ -1,9 +0,0 @@
-package com.labourhiring.springbootproject.LabourHiring.dao;
-
-public class Test2 {
-
-	public Test2() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
